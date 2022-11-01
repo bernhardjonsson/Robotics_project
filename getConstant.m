7cff0850548e94bca4b34c00c285308c47df2277
Zjllll00 <115778363+Zjllll00@users.noreply.github.com>
2022-11-01 16:00:49 +0100

    Add files via upload

--- getConstant.m
+++ getConstant.m
@@ -1,11 +1,10 @@
 function [MC] = getConstant(tin,tout,qin,qout,vin,vout,ain,aout)
-MC = [c1 c2 c3 c4 c5 c6]; 
 MT = [1 tin tin^2 tin^3 tin^4 tin^5;
        0 1 2*tin 3*tin^2 4*tin^4 5*tin^5;
        0 0 2  6*tin 12*tin^2 20*tin^3;
       1 tout tout^2 tout^3 tout^4 tout^5;
-       0 1 2*tout 3*tout^2 4*tout^4 5*tout^5;
+       0 1 2*tout 3*tout^2 4*tout^3 5*tout^4;
        0 0 2 6*tout 12*tout^2 20*tout^3];
-MQ = [qin,vin,ain,qout,vout,aout];
+MQ = [qin vin ain qout vout aout];
 MC = MT/MQ;
 end
