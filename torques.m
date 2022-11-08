ca73e12feaab609cab87e846e790cdd25f0de00a
Palli <gudbrandsson74@gmail.com>
2022-11-08 12:41:29 +0100

    Palli updates

--- torques.m
+++ torques.m
@@ -1,11 +1,21 @@
-function [torque2,torque3,torque4] = torques(t2,t3,t4)
-	% calculates the torques at each motor for a static position with the given angles and a 1N gravitational force upon the tip of the end effector(frame 4)
+function [t2,t3,t4] = torques(q2,q3,q4,F)
+	% calculates the torques at each motor for a static position with the given angles and a FN gravitational force upon the tip of the end effector(frame 4)
+	% this function will then spit out Nmm
 
-lengthFactor = sin(sqrt(t2^2+t3^2+t4^2));
-Torque = 1;
+L12 = 93;
+L23 = 93;
+L34 = 50;
 
-torque4 = Torque * lengthFactor * (L34);
-torque3 = Torque * lengthFactor * (L23 + L34);
-torque2 = Torque * lengthFactor * (L12 + L23 + L34);
+r3 = L34*sin(q2+q3+q4);
+r2 = r3 + L23*sin(q2+q3);
+r1 = r3 + r2 +L12*sin(q2);
+
+t4 = F*r3;
+t3 = F*r2;
+t2 = F*r1;
+
+
+
+
 
 
