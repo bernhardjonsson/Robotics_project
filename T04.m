043b4466055445e71e84f3ccac30687ad3091740
tarasz98 <tarasz98@gmail.com>
2022-11-08 13:28:21 +0100

    Update T04.m

--- T04.m
+++ T04.m
@@ -7,7 +7,7 @@ e1 = sind(theta4)*(cosd(theta3)*(cosd(theta1)*sind(theta2) + cosd(theta2)*sind(t
 
 e3 = cosd(theta3)*(cosd(theta1)*cosd(theta2) - sind(theta1)*sind(theta2)) - sind(theta3)*(cosd(theta1)*sind(theta2) + cosd(theta2)*sind(theta1));
 
-e2 = -cosd(theta4)*(cosd(theta3)*(cosd(theta1)*sind(theta2) + cosd(theta2)*sind(theta1)) + sind(theta3)*(cosd(theta1)*cosd(theta2) - sind(theta1)*sind(theta2))) - sind(theta4)}e3;
+e2 = -cosd(theta4)*(cosd(theta3)*(cosd(theta1)*sind(theta2) + cosd(theta2)*sind(theta1)) + sind(theta3)*(cosd(theta1)*cosd(theta2) - sind(theta1)*sind(theta2))) - sind(theta4)*e3;
 
 T = [e2, e1 - cosd(theta4)*e3, 0, 93*cosd(theta1) - 93*cosd(theta1)*sind(theta2) - 93*cosd(theta2)*sind(theta1);
      0, 0, -1, -50;
