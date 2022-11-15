320d386d518991cbcf18e9b333fb421d189554b3
bernhardjonsson <bernton96@gmail.com>
2022-11-15 16:00:49 +0100

    added solution for 6&7

--- getConstant.m
+++ getConstant.m
@@ -1,10 +1,14 @@
-function [MC] = getConstant(tin,tout,qin,qout,vin,vout,ain,aout)
-MT = [1 tin tin^2 tin^3 tin^4 tin^5;
-       0 1 2*tin 3*tin^2 4*tin^4 5*tin^5;
-       0 0 2  6*tin 12*tin^2 20*tin^3;
-      1 tout tout^2 tout^3 tout^4 tout^5;
-       0 1 2*tout 3*tout^2 4*tout^3 5*tout^4;
-       0 0 2 6*tout 12*tout^2 20*tout^3];
-MQ = [qin vin ain qout vout aout];
-MC = MT/MQ;
+function c = getConstant(t_in,t_out,qin,qout,vin,vout,ain,aout)
+A = [1, t_in, t_in^2, t_in^3, t_in^4, t_in^5;
+     0, 1, 2*t_in, 3*t_in^2, 4*t_in^3, 5*t_in^4;
+     0, 0, 2, 6*t_in, 12*t_in^2, 20*t_in^3;
+     1, t_out, t_out^2, t_out^3, t_out^4, t_out^5;
+     0, 1, 2*t_out, 3*t_out^2, 4*t_out^3, 5*t_out^4;
+     0, 0, 2, 6*t_out, 12*t_out^2, 20*t_out^3];
+
+c = zeros(6,4);
+for i = 1:4
+    b = [qin(i);vin(i);ain(i);qout(i);vout(i);aout(i)];
+    c(:,i)=A\b;
+end
 end
--- invKinematics.m
+++ invKinematics.m
@@ -14,10 +14,13 @@ o_03 = o_04 - a3*x_04;
 
 r_13 = sqrt(o_03(1)^2 + (o_03(3)-a0)^2);
 
-theta3 = acosd((-a1^2 - a2^2 + r_13^2)/(2*a1*a2));
+
+theta3 = acosd((a1^2 + a2^2 - r_13^2)/(2*a1*a2))
 
 phi = atan2(o_03(3) - a0, o_03(3));
 
+a2*sin(theta3)
+a1 + a2*cos(theta3)
 psi = atan2(a2*sin(theta3), a1 + a2*cos(theta3));
 
 theta2 = phi + psi;
