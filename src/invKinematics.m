622490e9fcfe271405b2d13ec330bcdc2fe0fc31
tarasz98 <tarasz98@gmail.com>
2022-11-17 11:14:21 +0100

    invKin

--- src/invKinematics.m
+++ src/invKinematics.m
@@ -12,7 +12,9 @@ theta1 = atan2(o_04(2), o_04(1));
 
 o_03 = o_04 - a3*x_04;
 
-r_13 = sqrt(o_03(1)^2 + (o_03(3)-a0)^2);
+r3 = sqrt(o_03(1)^2 + o_03(2)^2);
+
+r_13 = sqrt(r3^2 + (o_03(3)-a0)^2);
 
 value = (a1^2 + a2^2 - r_13^2)/(2*a1*a2);
 
@@ -29,7 +31,7 @@ end
 
 theta3 = acos(value);
 
-phi = atan2(o_03(3) - a0, o_03(3));
+phi = atan2(o_03(3) - a0, r3);
 
 a2*sin(theta3);
 a1 + a2*cos(theta3);
