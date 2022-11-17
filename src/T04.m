f7cf2d02362a6cb8c83edf12112505791cf3f9b7
tarasz98 <tarasz98@gmail.com>
2022-11-17 11:28:48 +0100

    T04

--- src/T04.m
+++ src/T04.m
@@ -5,16 +5,13 @@ function T = T04(theta1, theta2, theta3, theta4)
 
 e1 = sind(theta2 + theta3 + theta4);
 
-e2 = cosd(theta2 + theta3 + theta4);
+e3 = cosd(theta2 + theta3 + theta4);
 
-e4 = 93*sind(theta2)*sind(theta3);
+e2 = 50*e3 + 93*cosd(theta2 + theta3) + 93*cosd(theta2);
 
-e5 = 93*cosd(theta2)*cosd(theta3);
 
-e3 = e4 - e5 - 93*sind(theta2) + 50*cosd(theta2)*sind(theta3)*sind(theta4) + 50*cosd(theta3)*sind(theta2)*sind(theta4) + 50*sind(theta2)*sind(theta3)*sind(theta4) - 50*cosd(theta2)*cosd(theta3)*sind(theta4);
-
-T = [e2*cosd(theta1), -e1*cosd(theta1), sind(theta1), -cosd(theta1)*e3;
-     e2*sind(theta1), -e1*sind(theta1), -cosd(theta1), -sind(theta1)*e3;
-     e1, e2, 0, 93*sind(theta1) + e5 + e4 + 50*sind(theta4)*(cosd(theta2)*sind(theta3) + cosd(theta3)*sind(theta2)) + 50*sind(theta4)*(cosd(theta2)*cosd(theta3) - sind(theta2)*sind(theta3)) + 50;
+T = [e3*cosd(theta1), -e1*cosd(theta1), sind(theta1), cosd(theta1)*e2;
+     e3*sind(theta1), -e1*sind(theta1), -cosd(theta1), -sind(theta1)*e2;
+     e1, e3, 0, 50*e1 + 93*sind(theta2 + theta3) + 93*sin(theta2) + 50;
      0, 0, 0, 1];
 end
