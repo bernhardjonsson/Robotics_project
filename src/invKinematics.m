c5209974a0f0df587be9dd5e84c5c3a18941757e
bernhardjonsson <bernton96@gmail.com>
2022-11-17 14:37:19 +0100

    fixing some issues

--- src/invKinematics.m
+++ src/invKinematics.m
@@ -3,7 +3,7 @@ function [theta1, theta2, theta3, theta4] = invKinematics(x_04, o_04)
 % TEST THE INVERSE KINEMATICS
 
 % Lengths
-a0 = 50;
+a0 = 0;
 a1 = 93;
 a2 = 93;
 a3 = 50;
@@ -13,19 +13,17 @@ theta1 = atan2(o_04(2), o_04(1));
 o_03 = o_04 - a3*x_04;
 
 r3 = sqrt(o_03(1)^2 + o_03(2)^2);
-r_13 = sqrt(r3^2 + (o_03(3)-a0)^2);
+r_13 = sqrt(r3^2 + (o_03(3)-a0)^2)*-1;
 
-value = (a1^2 + a2^2 - r_13^2)/(2*a1*a2);
 
 theta3 = acosd((-a1^2 - a2^2 + r_13^2)/(2*a1*a2));
-%theta3 = real(theta3)
-phi = atan2(o_03(3) - a0, r3);
+phi = atan2d(o_03(3) - a0, r3);
 
-psi = atan2(a2*sin(theta3), a1 + a2*cos(theta3));
+psi = atan2d(a2*sind(-theta3), a1 + a2*cosd(-theta3));
 
 theta2 = phi + psi;
 
-theta234 = atan2(x_04(3), sqrt(1-x_04(3)^2));
+theta234 = atan2d(x_04(3), sqrt(1-x_04(3)^2));
 
 theta4 = theta234 - theta3 - theta2;
 
