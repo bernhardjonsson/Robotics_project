6a59b03a07cfc3e1a98412b3770dd814030b3943
tarasz98 <tarasz98@gmail.com>
2022-11-17 11:25:18 +0100

    T_finder

--- src/T_finder.m
+++ src/T_finder.m
@@ -11,26 +11,26 @@ L45 = 45;
 L35 = 35;
 
 % {0} -> {1}
-T01 = [cos(theta0), 0, sin(theta0), 0;
-       sin(theta0), 0, -cos(theta0), 0;
+T01 = [cosd(theta0), 0, sind(theta0), 0;
+       sind(theta0), 0, -cosd(theta0), 0;
        0, 1, 0, L01;
        0, 0, 0, 1];
 
 % {1} -> {2}
-T12 = [cos(theta1), -sin(theta1), 0, L12*cos(theta1);
-       sin(theta1), cos(theta1), 0, L12*sin(theta1);
+T12 = [cosd(theta1), -sind(theta1), 0, L12*cosd(theta1);
+       sind(theta1), cosd(theta1), 0, L12*sind(theta1);
        0, 0, 1, 0;
        0, 0, 0, 1];
 
 % {2} -> {3}
-T23 = [cos(theta2), -sin(theta2), 0, L23*cos(theta2);
-       sin(theta2), cos(theta2), 0, L23*sin(theta2);
+T23 = [cosd(theta2), -sind(theta2), 0, L23*cosd(theta2);
+       sind(theta2), cosd(theta2), 0, L23*sind(theta2);
        0, 0, 1, 0;
        0, 0, 0, 1];
 
 % {3} -> {4}     
-T34 = [cos(theta3), -sin(theta3), 0, L34*cos(theta3);
-       sin(theta3), cos(theta3), 0, L34*sin(theta3);
+T34 = [cosd(theta3), -sind(theta3), 0, L34*cosd(theta3);
+       sind(theta3), cosd(theta3), 0, L34*sind(theta3);
        0, 0, 1, 0;
        0, 0, 0, 1];    
 
