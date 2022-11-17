29caa84e12d86263cb75492ef4c7135eb87a69da
tarasz98 <tarasz98@gmail.com>
2022-11-17 11:16:31 +0100

    T_finder

--- src/T_finder.m
+++ src/T_finder.m
@@ -17,19 +17,19 @@ T01 = [cos(theta0), 0, sin(theta0), 0;
        0, 0, 0, 1];
 
 % {1} -> {2}
-T12 = [cos(theta1), -sin(theta1), 0, L12*sin(theta1);
+T12 = [cos(theta1), -sin(theta1), 0, L12*cos(theta1);
        sin(theta1), cos(theta1), 0, L12*sin(theta1);
        0, 0, 1, 0;
        0, 0, 0, 1];
 
 % {2} -> {3}
-T23 = [cos(theta2), -sin(theta2), 0, L23*sin(theta2);
+T23 = [cos(theta2), -sin(theta2), 0, L23*cos(theta2);
        sin(theta2), cos(theta2), 0, L23*sin(theta2);
        0, 0, 1, 0;
        0, 0, 0, 1];
 
 % {3} -> {4}     
-T34 = [cos(theta3), -sin(theta3), 0, L34*sin(theta3);
+T34 = [cos(theta3), -sin(theta3), 0, L34*cos(theta3);
        sin(theta3), cos(theta3), 0, L34*sin(theta3);
        0, 0, 1, 0;
        0, 0, 0, 1];    
