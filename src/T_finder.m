63fce178a1937e35a9f9d1a4619cd4101607ecca
bernhardjonsson <bernton96@gmail.com>
2022-11-17 09:55:00 +0100

    working on some issues

--- src/T_finder.m
+++ src/T_finder.m
@@ -1,8 +1,6 @@
 function [T01, T12, T23, T34, T35] = T_finder(theta0,theta1,theta2,theta3)
 % Finds the decimal representation of all the T matrixes based on the current angles of the joints
 
-
-
 L01 = 50;
 L12 = 93;
 L23 = 93;
@@ -17,19 +15,19 @@ T01 = [cos(theta0), 0, sin(theta0), 0;
        0, 0, 0, 1];
 
 % {1} -> {2}
-T12 = [cos(theta1), -sin(theta1), 0, L12*sin(theta1);
+T12 = [cos(theta1), -sin(theta1), 0, L12*cos(theta1);
        sin(theta1), cos(theta1), 0, L12*sin(theta1);
        0, 0, 1, 0;
        0, 0, 0, 1];
 
 % {2} -> {3}
-T23 = [cos(theta2), -sin(theta2), 0, L23*sin(theta2);
+T23 = [cos(theta2), -sin(theta2), 0, L23*cos(theta2);
        sin(theta2), cos(theta2), 0, L23*sin(theta2);
        0, 0, 1, 0;
        0, 0, 0, 1];
 
 % {3} -> {4}     
-T34 = [cos(theta3), -sin(theta3), 0, L34*sin(theta3);
+T34 = [cos(theta3), -sin(theta3), 0, L34*cos(theta3);
        sin(theta3), cos(theta3), 0, L34*sin(theta3);
        0, 0, 1, 0;
        0, 0, 0, 1];    
--- src/getConstant3.m
+++
@@ -1,8 +0,0 @@
-function [MC] = getConstant3(tin,tout,qin,qout,vin,vout)
-MT = [1 tin tin^2 tin^3;
-       0 1 2*tin 3*tin^2;
-      1 tout tout^2 tout^3;
-       0 1 2*tout 3*tout^2];
-MQ = [qin vin qout vout];
-MC = MT/MQ;
-end
